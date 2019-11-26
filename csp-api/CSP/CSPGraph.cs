6736cec75534c1ab36409c731b45b053963b0fd9
Mason Reynolds <masonreynolds65@gmail.com>
2019-11-26 18:05:25 -0500

    Added inline comments

--- csp-api/CSP/CSPGraph.cs
+++ csp-api/CSP/CSPGraph.cs
@@ -7,35 +7,47 @@ namespace csp_api.CSP
 {
 	public class CSPGraph
 	{
+		// Lists of vertices and edges present in the graph
 		public List<Vertex> Vertices { get; set; }
 		public List<Edge> Edges { get; set; }
 
+		// Function definitions for each method of choosing variables or values
 		private Func<Dictionary<string, byte>, CSPGraph, Vertex> _selectUnassignedVariable;
 		private Func<Vertex, Dictionary<string, byte>, CSPGraph, List<byte>> _orderDomainValues;
 		private Func<Vertex, byte, Dictionary<string, byte>, CSPGraph, bool>? _inference;
 
+		// Constructor for the CSP given the following arguments
 		internal CSPGraph(List<string> nodes, Dictionary<string, List<string>> constraints, List<string> domain,
 			Func<Dictionary<string, byte>, CSPGraph, Vertex> selectUnassignedVariable,
 			Func<Vertex, Dictionary<string, byte>, CSPGraph, List<byte>> orderDomainValues,
 			Func<Vertex, byte, Dictionary<string, byte>, CSPGraph, bool>? inference)
 		{
+			// Create a new list of vertices based on the nodes passed in
 			this.Vertices = nodes.Select(node => new Vertex(node, Enumerable.Range(0, domain.Count).
 							Select(num => (byte)num).ToList())).ToList();
+
+			// Create a new, empty list for the edges
 			this.Edges = new List<Edge>();
 
+			// Initialize the edges based on the constraints passed in
 			_initEdges(constraints);
 
+			// Initialize the functions
 			this._selectUnassignedVariable = selectUnassignedVariable;
 			this._orderDomainValues = orderDomainValues;
 			this._inference = inference;
 		}
 
+		// Method to create the edges based on a dictionary of constraints
 		private void _initEdges(Dictionary<string, List<string>> constraints)
 		{
+			// For each vertex in the graph
 			foreach (var v in Vertices)
 			{
+				// For each neighbor of the current vertex
 				foreach (var neighbor in constraints[v.Element])
 				{
+					// Store the neighboring vertex
 					var nv = this.Vertices.Find(vert => vert.Element == neighbor);
 
 					if (nv == null)
@@ -43,115 +55,170 @@ namespace csp_api.CSP
 						throw new Exception("A vertex is null, this should not happen");
 					}
 
+					// Store a new edge between the current vertex and its neighbor
 					var newEdge = new Edge(v, nv);
 
+					// Add the edge to the current vertex and the CSP graph
 					v.addEdge(newEdge);
 					this.Edges.Add(newEdge);
 				}
 			}
 		}
 
+		// Method to get the result of creating the CSP graph
 		public CSPResult RunCSP()
 		{
+			// Assume the program will no succeed
 			var success = false;
+
+			// Initialize the list of assignments for the values of each vertex
 			var assignments = new Dictionary<string, byte>();
+
+			// Initialize the number of backtracks done during the creation of the CSP graph
 			var numBacktracks = 0;
+
+			// Create a stop watch to record the time
 			var stopwatch = new Stopwatch();
 
+			// Start the stop watch
 			stopwatch.Start();
+
+			// Get the success of creating the CSP graph
 			success = _cspHelper(assignments, ref numBacktracks);
+
+			// Stop the stopwatch
 			stopwatch.Stop();
 
+			// Get the elapsed time from the stop watch
 			var elapsedTime = stopwatch.ElapsedMilliseconds;
 
+			// Get the results from the CSP after it has been created
 			var results = new CSPResult(success, assignments, numBacktracks, elapsedTime);
 
+			// Return the results 
 			return results;
 		}
 
+		// Method to create a CSP graph based on the options chosen by the user
 		private bool _cspHelper(Dictionary<string, byte> assignments, ref int numBacktracks)
 		{
+			// If all vertices have been assigned a value
 			if (assignments.Count == this.Vertices.Count)
 			{
+				// Return true
 				return true;
 			}
 
+			// Get the next unassigned value from the graph
 			var unassigned = _selectUnassignedVariable(assignments, this);
 
+			// For each value from the ordered domain of the unassigned vertex
 			foreach (var value in _orderDomainValues(unassigned, assignments, this))
 			{
+				// Initialize the previous domain to null
 				Dictionary<string, List<byte>>? previousDomain = null;
 
+				// If the inference method is not initialized
 				if (_inference != null)
 				{
+					// Assign the current value to the unassigned vertex
 					_assign(assignments, unassigned.Element, value);
+
+					// Get the current domain from the current list of vertices
 					previousDomain = this.Vertices.ToDictionary(key => key.Element,
 																value => new List<byte>(value.Domain));
 
+					// Get the success of the inference step 
 					var inferenceSuccess = _inference(unassigned, value, assignments, this);
 
+					// If the inference succeeded 
 					if (inferenceSuccess)
 					{
+						// Call this method again recursively with the new set of assignments
 						var success = _cspHelper(assignments, ref numBacktracks);
 
+						// If a solution was found
 						if (success)
 						{
+							// Return true
 							return true;
 						}
 					}
 
+					// Backtrack since a solution could not be found with the current assignments
 					_backtrack(assignments, previousDomain, ref unassigned, ref numBacktracks);
 				}
+				// Else the inference method is initialized
 				else
 				{
+					// If the current value is consistent with the other assignments
 					if (_isConsistent(unassigned, value, assignments))
 					{
+						// Assign the current value to the unassigned vertex
 						_assign(assignments, unassigned.Element, value);
+
+						// Call this method again recursively with the new set of assignments
 						var success = _cspHelper(assignments, ref numBacktracks);
 
+						// If a solution was found
 						if (success)
 						{
 							return true;
 						}
 
+						// Backtrack since a solution could not be found with the current assignments
 						_backtrack(assignments, previousDomain, ref unassigned, ref numBacktracks);
 					}
 				}
 
 			}
 
+			// Return false if no values could be used with the unassigned vertex
 			return false;
 		}
 
+		// Method to check if a vertex assignment is consistent with its neighbors
 		private bool _isConsistent(Vertex unassigned, byte value, Dictionary<string, byte> assignments)
 		{
+			// For each edge in the unassigned vertex
 			foreach (var edge in unassigned.Edges)
 			{
+				// If the neighbor is already assigned and has the same value
 				if (assignments.ContainsKey(edge.EndVertices.end.Element) &&
 					assignments[edge.EndVertices.end.Element] == value)
 				{
+					// Return false
 					return false;
 				}
 			}
 
+			// Return true
 			return true;
 		}
 
+		// Method to assign a value to a particular vertex
 		private void _assign(Dictionary<string, byte> assignments, string element, byte value)
 		{
 			assignments.Add(element, value);
 		}
 
+		// Method to backtrack an assignment and update the domain
 		private void _backtrack(Dictionary<string, byte> assignments,
 			Dictionary<string, List<byte>>? previousDomain, ref Vertex unassigned, ref int numBacktracks)
 		{
+			// Increment the number of backtracks
 			numBacktracks++;
+
+			// Unassign the vertex
 			assignments.Remove(unassigned.Element);
 
+			// If the previous domain is not null
 			if (previousDomain != null)
 			{
+				// For each vertex in the CSP graph
 				foreach (var v in this.Vertices)
 				{
+					// Revert the domain of the current vertex
 					v.Domain = previousDomain[v.Element];
 				}
 			}
--- csp-api/CSP/CSPGraphFactory.cs
+++ csp-api/CSP/CSPGraphFactory.cs
@@ -7,81 +7,123 @@ namespace csp_api.CSP
 {
 	public static class CSPGraphFactory
 	{
+		// Method to build the methods to be used during the creation of the CSP graph
+		// based on the options chosen by the user
 		public static CSPGraph BuildCSPGraph(BuildCSPGraphInput inputModel)
 		{
+			// Declaration for the function to select the next vertex to be assigned
 			Func<Dictionary<string, byte>, CSPGraph, Vertex> selectUnassignedVariable;
 
+			// If the user chose MRV and Degree Constraint
 			if (inputModel.MRV && inputModel.DC)
 			{
 				selectUnassignedVariable = (assignments, cspGraph) =>
 				{
+					// Get a list of the unassigned vertices in the graph
 					var unassigned = cspGraph.Vertices.Where(vertex => !assignments.ContainsKey(vertex.Element)).ToList();
 
+					// Create a list of the minimum unassigned values
 					var minUnassigned = new List<Vertex>();
+
+					// Add the first unassigned vertex to the list
 					minUnassigned.Add(unassigned[0]);
+
+					// Store the number of values in the domain of the current vertex
 					var minLegalValues = unassigned[0].Domain.Count;
 
+					// For each unassigned vertex
 					foreach (var vertex in unassigned)
 					{
+						// Get the assignments of the neighboring vertices
 						var takenValues = vertex.Edges.Where(edge => assignments.ContainsKey(edge.EndVertices.end.Element))
 														.Select(edge => assignments[edge.EndVertices.end.Element])
 														.Where(assignment => vertex.Domain.Contains(assignment))
 														.Distinct().ToList();
 
+						// Calculate the number of legal values for the current vertex
 						var legalValues = vertex.Domain.Count - takenValues.Count;
 
+						// If the number of legal values is equal to the number of minimum legal values
 						if (legalValues == minLegalValues)
 						{
+							// Add the current vertex to the list of minimum vertices
 							minUnassigned.Add(vertex);
 						}
+						// Else if the number of legal values is less than the current minimum
+						// number of legal values
 						else if (legalValues < minLegalValues)
 						{
+							// Clear the list of minimum vertices
 							minUnassigned.Clear();
+
+							// Add the current vetex to the list of minimum vertices
 							minUnassigned.Add(vertex);
+
+							// Update the minimum number of legal values
 							minLegalValues = legalValues;
 						}
 					}
 
+					// Set the maximum unassigned vertex equal to the first in the minimum list
 					var maxUnassigned = minUnassigned[0];
 
+					// If the number of minimum vertices is greater than one
 					if (minUnassigned.Count > 1)
 					{
+						// Get the edges of the first vertex
 						var maxEdges = maxUnassigned.Edges.Count;
 
+						// For each minimum vertex
 						foreach (var vertex in minUnassigned)
 						{
+							// If the current vertex has more edges than the current max
 							if (vertex.Edges.Count > maxEdges)
 							{
+								// Update the max vertex and the max edges
 								maxUnassigned = vertex;
 								maxEdges = vertex.Edges.Count;
 							}
 						}
 					}
 
+					// return the max vertex of the minimum domains
 					return maxUnassigned;
 				};
 			}
+			// Else if the user only chose MRV
 			else if (inputModel.MRV)
 			{
 				selectUnassignedVariable = (assignments, cspGraph) =>
 				{
+					// Get a list of the unassigned vertices in the graph
 					var unassigned = cspGraph.Vertices.Where(vertex => !assignments.ContainsKey(vertex.Element)).ToList();
 
+					// Get the first vertex
 					var minUnassigned = unassigned[0];
+
+					// Store the number of values in the domain of the current vertex
 					var minLegalValues = unassigned[0].Domain.Count;
 
+					// For each unassigned vertex
 					foreach (var vertex in unassigned)
 					{
+						// Get the assignments of the neighboring vertices
 						var takenValues = vertex.Edges.Where(edge => assignments.ContainsKey(edge.EndVertices.end.Element))
 														.Select(edge => assignments[edge.EndVertices.end.Element])
 														.Where(assignment => vertex.Domain.Contains(assignment))
 														.Distinct().ToList();
 
+						// Calculate the number of legal values for the current vertex
 						var legalValues = vertex.Domain.Count - takenValues.Count;
 
+						// If the number of legal values is less than the current minimum
+						// number of legal values
 						if (legalValues < minLegalValues)
 						{
+							// Add the current vetex to the list of minimum vertices
 							minUnassigned = vertex;
+
+							// Update the minimum number of legal values
 							minLegalValues = legalValues;
 						}
 					}
@@ -89,65 +131,61 @@ namespace csp_api.CSP
 					return minUnassigned;
 				};
 			}
-			else if (inputModel.DC)
-			{
-				selectUnassignedVariable = (assignments, cspGraph) =>
-				{
-					var unassigned = cspGraph.Vertices.Where(vertex => !assignments.ContainsKey(vertex.Element)).ToList();
-
-					var maxUnassigned = unassigned[0];
-					var maxEdges = maxUnassigned.Edges.Count;
-
-					foreach (var vertex in unassigned)
-					{
-						if (vertex.Edges.Count > maxEdges)
-						{
-							maxUnassigned = vertex;
-							maxEdges = vertex.Edges.Count;
-						}
-					}
-
-					return maxUnassigned;
-				};
-			}
+			// Else the user chose neither
 			else
 			{
 				selectUnassignedVariable = (assignments, cspGraph) =>
 				{
+					// Find the first unassigned vertex in the CSP graph
 					var unassigned = cspGraph.Vertices.Find(vertex => !assignments.ContainsKey(vertex.Element));
+					
 					if (unassigned == null)
 					{
 						throw new Exception("Unassigned is not defined");
 					}
 
+					// Return the first unassigned vertex
 					return unassigned;
 				};
 			}
 
+			// Initialize function order domains based on the options chosen by the user
 			Func<Vertex, Dictionary<string, byte>, CSPGraph, List<byte>> orderDomainValues;
 
+			// If the user chose LCV
 			if (inputModel.LCV)
 			{
 				orderDomainValues = (unassigned, assignments, cspGraph) =>
 				{
+					// Get the unassigned neighbors of the unassigned vertex
 					var neighbors = unassigned.Edges.Where(edge => !assignments.ContainsKey(edge.EndVertices.end.Element))
 												.Select(edge => edge.EndVertices.end).ToList();
 
+					// Create an array of integers for each value in the domain of the unassigned vertex
 					var lcvHeuristics = new int[unassigned.Domain.Count];
 
+					// For every value in the domain of the unassigned vertex
 					for (int i = 0; i < unassigned.Domain.Count; i++)
 					{
+						// Set the current heuristic of the domain to zero
 						lcvHeuristics[i] = 0;
+
+						// For each neighbor of the unassigned vertex
 						foreach (var neighbor in neighbors)
 						{
+							// If the neighbor and the unassigned vertex share a value
 							if (neighbor.Domain.Contains(unassigned.Domain[i]))
 							{
+								// Increment the heuristic of this domain value
 								++lcvHeuristics[i];
 							}
 						}
 					}
 
+					// Create a list of the domains to be sorted
 					var sortedDomain = new List<byte>(unassigned.Domain);
+
+					// Perform bubble sort on the list
 					for (int i = 0; i < lcvHeuristics.Length - 1; i++)
 					{
 						for (int j = 0; j < lcvHeuristics.Length - i - 1; j++)
@@ -165,106 +203,141 @@ namespace csp_api.CSP
 						}
 					}
 
+					// Return the sorted domain
 					return sortedDomain;
 				};
 			}
+			// Else the user did not choose LCV
 			else
 			{
 				orderDomainValues = (unassigned, assignments, cspGraph) =>
 				{
+					// Return the original domain
 					return unassigned.Domain;
 				};
 			}
 
+			// Initialze function to perform the inference step in forward checking
 			Func<Vertex, byte, Dictionary<string, byte>, CSPGraph, bool>? inference = null;
 
+			// If the user chose forward checking
 			if (inputModel.ForwardChecking)
 			{
+				// Initialized function to perform propogation
 				Func<Dictionary<string, byte>, CSPGraph, bool> propogate;
 
+				// If the user chose propogation
 				if (inputModel.Propagation)
 				{
 					propogate = (assignments, cspGraph) =>
 					{
-						// AC-3 Algorithm
-
+						// Store all of the edges in the CSP in a queue
 						var edgesQueue = new Queue<Edge>(cspGraph.Edges);
 
+						// While there are still edges in the queue
 						while (edgesQueue.Any())
 						{
+							// Get the next edge from the queue
 							var currentEdge = edgesQueue.Dequeue();
 
-							// Remove-Inconsistent-Values
+							// Get the start and end vertices of the current edge
 							var (vertexX, vertexY) = currentEdge.EndVertices;
 
+							// If the starting vertex of the current edge is assigned or 
+							// the ending vertex has more the one value in its domain
 							if (assignments.ContainsKey(vertexX.Element) || vertexY.Domain.Count > 1)
 							{
+								// Continue to the next iteration
 								continue;
 							}
 
+							// Set removed equal to false and the removed element as zero
 							var removed = false;
 							byte removedElement = 0;
 
+							// For each element in the domain of the current starting vertex
 							foreach (var element in vertexX.Domain)
 							{
+								// If the first domain of the ending vertex is equal to the current element
 								if (vertexY.Domain[0] == element)
 								{
+									// Set removed equal to true and store the current element
 									removed = true;
 									removedElement = element;
+
+									// Break from the for loop
 									break;
 								}
 							}
 
+							// If any values need to be removed
 							if (removed)
 							{
+								// Remove the value from the starting vertex's domain
 								vertexX.Domain.Remove(removedElement);
 
+								// If there are no values in the domain
 								if (!vertexX.Domain.Any())
 								{
+									// Return false
 									return false;
 								}
 
+								// Get the unassigned neighbors of the starting vertex
 								var neighbors = vertexX.Edges.Where(edge => !assignments.ContainsKey(edge.EndVertices.end.Element));
 
+								// Foreach neighbor in neighbors
 								foreach (var neighbor in neighbors)
 								{
+									// Add all of the neighbors back into the queue in reversed order
 									edgesQueue.Enqueue(new Edge(neighbor.EndVertices.end, neighbor.EndVertices.start));
 								}
 							}
 						}
 
+						// Return true
 						return true;
 					};
 				}
+				// Else the user did not choose propogate
 				else
 				{
 					propogate = (assignments, cspGraph) =>
 					{
+						// Return true
 						return true;
 					};
 				}
 
 				inference = (unassigned, value, assignments, cspGraph) =>
 				{
+					// Get the unassigned neighbors of the unassigned vertex
 					var neighbors = unassigned.Edges.Where(edge => !assignments.ContainsKey(edge.EndVertices.end.Element))
 												.Select(edge => edge.EndVertices.end).ToList();
 
+					// For each neighbor in neighbors
 					foreach (var neighbor in neighbors)
 					{
+						// Remove the value from the domain of the current neighbor
 						neighbor.Domain.Remove(value);
 
+						// If there are no values in the current neighbor's domain
 						if (neighbor.Domain.Count == 0)
 						{
+							/// Return false
 							return false;
 						}
 					}
 
+					// Run the propogate method
 					var valid = propogate(assignments, cspGraph);
 
+					// Return the result of propogation
 					return valid;
 				};
 			}
 
+			// Return a new CSP graph with the corresponding methods and structure
 			return new CSPGraph(inputModel.States, inputModel.Constraints, inputModel.Colors, selectUnassignedVariable, orderDomainValues, inference);
 		}
 	}
